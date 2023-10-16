6b51ca4178e184f5918e7c116adc61074ee20e80
sajan26v <sajanthanv26gmail.com>
2023-10-16 20:07:37 +0530

    modified tut-03.py

---
+++ tut-03.py
@@ -0,0 +1,12 @@
+# Question 01 
+# Part a
+try:
+    n = int(input("Give me a number: "))
+    if n == 100:
+        print(n, "is equal to 100")
+    elif n > 100:
+        print(n, "is greater than 100")
+    else:
+        print(n, "is less than 100")
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
