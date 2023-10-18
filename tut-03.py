00914d2437c6a523376c8511604ec224f4a7d6ff
sajan26v <sajanthanv26@gmail.com>
2023-10-18 10:00:47 +0530

    modified tut-03.py

--- tut-03.py
+++ tut-03.py
@@ -1,14 +1,27 @@
 # Question 01 
 # Part a
-try:
-    n = int(input("Give me a number: "))
-    if n == 100:
-        print(n, "is equal to 100")
-    elif n > 100:
-        print(n, "is greater than 100")
-    else:
-        print(n, "is less than 100")
-except ValueError:
-    print("Invalid input. Please enter a valid number.")
+"""while True:
+    try:
+        n = int(input("Give me a number: "))
+        if n == 100:
+            print(n, "is equal to 100")
+        if n > 100:
+            print(n, "is greater than 100")
+        if n < 100:
+            print(n, "is less than 100")
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")"""
 
 #Part b
+"""while True:
+    try:
+        age = int(input("Enter your Age: "))
+        
+        if age >= 18:
+            print("You Can Vote")
+        if age < 18:
+            print(" ")
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")"""
