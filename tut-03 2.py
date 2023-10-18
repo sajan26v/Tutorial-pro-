8ec12b44bc91cd9967e1e581e9424543443eadb5
sajan26v <sajanthanv26@gmail.com>
2023-10-18 10:32:11 +0530

    Added file tut-03 2.py

---
+++ tut-03 2.py
@@ -0,0 +1,27 @@
+#Question 02
+# part a
+"""while True:
+    try:
+        num = int(input("Enter the Number: "))
+        if num < 0:
+            print(num,"It's a Negative number")
+        else:
+            if num > 0:
+                print(num, "It's a Postive number")
+            else:
+                print(num,"It's neither Positive nor Negative ")
+        break
+    except ValueError:
+        print("Invaild Input: Please Enter Again")"""
+
+#Part b
+"""while True:
+    try:
+        mark = float(input("Enter Your Mark: "))
+        if mark < 40:
+            print(mark,"You are Failed")
+        else:
+            print(mark,"You are Passed")
+        break
+    except ValueError:
+        print("Invaild Marks: Please Enter Again.")"""
