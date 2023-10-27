4e62f21fb3ee708943cfde45a5950d567a1fa76e
sajan26v <sajanthanv26@gmail.com>
2023-10-27 11:39:18 +0530

    New File added Part06.py

---
+++ Part06.py
@@ -0,0 +1,25 @@
+#Question 1a
+"""while True:
+    try:
+        n = input('Give me a number over 100: ')
+        n = int(n)
+        if n <= 100:
+            print(n, "is not over than 100")
+            break
+        else:
+            print(n, "is over than  100")
+    except ValueError:
+       print('Invalid input. Please enter a valid integer value.')"""
+
+#Question 1b
+"""while True:
+    try:
+        age = input("Enter your age: ")
+        age = int(age)
+        if age >= 18:
+            print(age,"eligable for vote")
+            break
+        else:
+            print(age, "Not eligibale for vote")
+    except ValueError:
+        print("Invaild Output: Please enter Proper Age ")"""
